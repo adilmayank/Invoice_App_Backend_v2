3ceb987fd5411ef389b5f969b88e3f1d8610256c
adilmayank <adilmayank5894@gmail.com>
2023-04-11 18:15:45 +0530

    added payment term router

---
+++ Routers/paymentTermRouter.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const router = express.Router()
+const { PaymentTermController } = require('../Controllers')
+const { PaymentTermService } = require('../Services')
+const { PaymentTermRepository } = require('../Data Access')
+const { PaymentTermModel } = require('../Models')
+
+// Dependency injection
+const paymentTermRepository = new PaymentTermRepository(PaymentTermModel)
+const paymentTermService = new PaymentTermService(paymentTermRepository)
+const paymentTermController = new PaymentTermController(paymentTermService)
+
+// get all payment terms
+router.get('/api/v2/payment-terms', paymentTermController.getAllPaymentTerms)
+
+// create new payment term
+router.post('/api/v2/payment-terms', paymentTermController.addPaymentTerm)
+
+// hard delete single payment term
+router.delete('/api/v2/payment-terms', paymentTermController.removePaymentTerm)
+
+module.exports = router
